d6a567f9867336d2369b8635178a236bb80c6f18
Ismail <ismail.mohamed@oracle.com>
2021-02-06 21:56:32 +0530

    test

---
+++ ReactTestScripts/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+import reportWebVitals from './reportWebVitals';
+import temp  from './test';
+//let city='madrid';
+
+ReactDOM.render(<h1>the city is {temp.name}</h1>,document.getElementById('root'));
+
+reportWebVitals();
